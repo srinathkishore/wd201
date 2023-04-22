b8363208ae9b65384ab743fcf98e86f530415d23
Srinath Kishore <srinathkishore.a@gmail.com>
2023-04-22 10:33:52 +0530

    lv9

--- todo-app/__tests__/todos.js
+++ todo-app/__tests__/todos.js
@@ -63,6 +63,36 @@ describe("Todo test suite ", () => {
     expect(parsedUpdateResponse.completed).toBe(true);
   });
 
+  test("Mark todo as incomplete (Updating Todo)", async () => {
+    let res = await agent.get("/");
+    let csrfToken = extractCsrfToken(res);
+    await agent.post("/todos").send({
+      title: "Clean the house",
+      dueDate: new Date().toISOString(),
+      completed: true,
+      _csrf: csrfToken,
+    });
+    const groupedTodosResponse = await agent
+      .get("/")
+      .set("Accept", "application/json");
+    const parsedGroupedResponse = JSON.parse(groupedTodosResponse.text);
+    const dueTodayCount = parsedGroupedResponse.dueToday?.length ?? 0;
+    if (dueTodayCount === 0) {
+      return;
+    }
+    const latestTodo = parsedGroupedResponse.dueToday[dueTodayCount - 1];
+    const status = latestTodo.completed ? false : true;
+    res = await agent.get("/");
+    csrfToken = extractCsrfToken(res);
+
+    const response = await agent.put(`todos/${latestTodo.id}`).send({
+      _csrf: csrfToken,
+      completed: status,
+    });
+    const parsedUpdateResponse = JSON.parse(response.text);
+    expect(parsedUpdateResponse.completed).toBe(false);
+  });
+
   test("Delete todo using ID", async () => {
     let res = await agent.get("/");
     let csrfToken = extractCsrfToken(res);
